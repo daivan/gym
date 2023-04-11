0ef7b7fe3e701ea3ac9f6bc14c1650f22b9d931e
daivan <daivan@gmail.com>
2023-04-11 17:14:39 +0200

    Added all classic control

---
+++ acrobot/run_model.py
@@ -0,0 +1,30 @@
+import gymnasium as gym
+import os
+
+# Check what descrete it is https://stable-baselines.readthedocs.io/en/master/guide/algos.html to see what algorithm to use
+from stable_baselines3 import PPO
+
+# Change these two to match your model and file
+models_dir = "models/ppo"
+model_file = "2980000"
+
+# change how many times it will play
+episodes = 10
+
+model_path = f"{models_dir}/{model_file}"
+
+
+env = gym.make('Acrobot-v1', render_mode="human")
+
+model = PPO.load(model_path, env=env)
+
+for episode in range(episodes):
+    observation = env.reset()
+    observation = observation[0]
+    done = False
+    while not done:
+        action, _ = model.predict(observation)
+        observation, reward, done, truncated, info = env.step(action)
+
+    
+env.close()
--- mountain_car/train.py
+++ mountain_car/train.py
@@ -1,9 +1,6 @@
 import gymnasium as gym
 import os
-
-# Check what descrete it is https://stable-baselines.readthedocs.io/en/master/guide/algos.html to see what algorithm to use
 from stable_baselines3 import PPO
-
 
 models_dir = "models/ppo"
 log_dir = "logs"
@@ -14,12 +11,9 @@ if not os.path.exists(models_dir):
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-
-#env = gym.make('LunarLander-v2', render_mode="human")
-env = gym.make('LunarLander-v2')
+env = gym.make('MountainCar-v0')
 
 env.reset()
-
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
 
 TIMESTEPS = 10000
@@ -27,11 +21,5 @@ TIMESTEPS = 10000
 for i in range(1, 300):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
     model.save(f"{models_dir}/{TIMESTEPS*i}")
-
-
-
-#print("sample step: ", env.action_space.sample())
-#print("sample observation shape:", env.observation_space.shape)
-#print("sample observation sample:", env.observation_space.sample())
 
 env.close()
