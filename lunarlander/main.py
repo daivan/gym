906c34c99134a9375488b60fe2d9dbd223ee5d60
daivan <daivan@gmail.com>
2023-04-11 12:27:12 +0200

    Lunar lander v2

--- lunarlander/main.py
+++ lunarlander/main.py
@@ -1,4 +1,37 @@
-import gym
-from stable_baselines import ACER
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines.common.evaluation import evaluate_policy
+import gymnasium as gym
+import os
+
+# Check what descrete it is https://stable-baselines.readthedocs.io/en/master/guide/algos.html to see what algorithm to use
+from stable_baselines3 import PPO
+
+
+models_dir = "models/ppo"
+log_dir = "logs"
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+
+#env = gym.make('LunarLander-v2', render_mode="human")
+env = gym.make('LunarLander-v2')
+
+env.reset()
+
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
+
+TIMESTEPS = 10000
+
+for i in range(1, 300):
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
+    model.save(f"{models_dir}/{TIMESTEPS*i}")
+
+
+
+#print("sample step: ", env.action_space.sample())
+#print("sample observation shape:", env.observation_space.shape)
+#print("sample observation sample:", env.observation_space.sample())
+
+env.close()
--- readme.md
+++ readme.md
@@ -10,5 +10,26 @@ pip install gymnasium[box2d]
 pip install torch torchvision torchaudio
 pip install stable-baselines3[extra] protobuf==3.20.*
 pip install mss pydirectinput
+pip install git+https://github.com/DLR-RM/stable-baselines3@feat/gymnasium-support # Installing this because stable_baseline 1.7.0 has trouble with gymnasium 0.28.1. https://github.com/DLR-RM/stable-baselines3/pull/780
 ```
 
+# Activate tensorboard
+to run the webserver for tensorboard go to the program that is currently logging and run this command
+```
+tensorboard --logdir=logs
+```
+Make sure that port 6006 is not in use. If it is, you can change the port with the --port flag.
+
+Where logs is the folder where the logs are currently generated.
+
+It will prompt you with this
+```
+Serving TensorBoard on localhost; to expose to the network, use a proxy or pass --bind_all
+TensorBoard 2.12.1 at http://localhost:6006/ (Press CTRL+C to quit)
+```
+
+# Start the program with a specific model
+Lets say that you have trained the model and you want to test it. Make sure you have a `run_model.py`
+```
+python run_model.py
+```
